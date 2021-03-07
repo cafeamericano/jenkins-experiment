672cbbffe571b3b3cbde2d6aeb62cb171693db74
unknown <mfarmer5102@gmail.com>
2021-03-06 23:33:53 -0500

    sticking with ejs for the time being

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ### Getting started
 
 1. In root directory, run `yarn start`
-2. In subOne/ directory, run `yarn start`
-3. Access localhost:9001/subOne to see how this microfront end gets displayed.
+2. In platform/ directory, run `yarn start`
+3. Access localhost:9001/platform to see how this microfront end gets displayed.
--- src/mfarmer5102-root-config.js
+++ src/mfarmer5102-root-config.js
@@ -1,33 +1,15 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//     name: "@mf/root-config",
-//     app: () => System.import("@mf/root-config"),
-//     activeWhen: ["/"],
-// });
-
-// registerApplication({
-//     name: "@LogRocket/logrocket-single-spa-homepage",
-//     app: () => System.import("@LogRocket/logrocket-single-spa-homepage"),
-//     activeWhen: [(location) => location.pathname === "/"],
-// });
-
-// registerApplication({
-//     name: "@LogRocket/logrocket-single-spa-aboutpage",
-//     app: () => System.import("@LogRocket/logrocket-single-spa-aboutpage"),
-//     activeWhen: ["/about"],
-// });
-
 registerApplication({
-    name: "@mfarmer5102/subOne",
-    app: () => System.import("@mfarmer5102/subOne"),
-    activeWhen: ["/subOne"],
+  name: "@mfarmer5102/platform",
+  app: () => System.import("@mfarmer5102/platform"),
+  activeWhen: ["/"],
 });
 
 registerApplication({
-    name: "@mfarmer5102/subTwo",
-    app: () => System.import("@mfarmer5102/subTwo"),
-    activeWhen: ["/subTwo"],
+  name: "@mfarmer5102/appContent",
+  app: () => System.import("@mfarmer5102/appContent"),
+  activeWhen: ["/appContent"],
 });
 
 start();
